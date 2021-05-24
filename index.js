b805320d7cca4a8a8a826024867648cabdb9b4be
AugustoCiuffoletti <augusto@di.unipi.it>
2021-05-24 22:21:55 +0200

    1 file modified

--- index.js
+++ index.js
@@ -7,24 +7,29 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 var baseURL =
   'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
-
-fetch(baseURL + '/new', { method: 'POST' } )
-.then(response => response.json(), error => alert(error))
-.then(key => {
-  console.log(key)
-  fetch(baseURL + '/post?key=' + key + '&msg=[{"titolo": "cane","messaggio":"comprare il cibo"}]', { method: 'POST' } )
+var postit = [
+  {
+    titolo: 'cane',
+    messaggio: 'comprare il cibo',
+    importante: true
+  }
+];
+var msg = JSON.stringify(postit);
+fetch(baseURL + '/new', { method: 'POST' })
   .then(response => response.json(), error => alert(error))
-  .then(data => { 
-    console.log(data);
-    fetch(baseURL + '/get?key=' + key)
-    .then(response => response.json(), error => alert(error))
-    .then(data => {
-      console.log(data);
-      var obj = JSON.parse(data);
-      console.log(obj[0].titolo);
-      console.log(obj[0].messaggio);
-    });                             
+  .then(key => {
+    console.log(key);
+    fetch(baseURL + '/post?key=' + key + '&msg=' + msg, { method: 'POST' })
+      .then(response => response.json(), error => alert(error))
+      .then(data => {
+        console.log(data);
+        fetch(baseURL + '/get?key=' + key)
+          .then(response => response.json(), error => alert(error))
+          .then(data => {
+            console.log(data);
+            var obj = JSON.parse(data);
+            console.log(obj[0].titolo);
+            console.log(obj[0].messaggio);
+          });
+      });
   });
-});
-
-
